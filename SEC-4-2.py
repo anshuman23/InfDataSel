ced6166133e6c68d79ae3bb79344be9400d01b7a
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2024-05-17 12:27:35 -0400

    Update SEC-4-2.py

--- SEC-4-2.py
+++ SEC-4-2.py
@@ -1,12 +1,12 @@
 import os
 
-os.system('python SEC-4-2.py --dataset adult --metric dp --seed 42')
-os.system('python SEC-4-2.py --dataset bank --metric dp --seed 42')
-os.system('python SEC-4-2.py --dataset celeba --metric dp --seed 42')
-os.system('python SEC-4-2.py --dataset nlp --metric dp --seed 42')
+os.system('python SEC-4-2-HELPER.py --dataset adult --metric dp --seed 42')
+os.system('python SEC-4-2-HELPER.py --dataset bank --metric dp --seed 42')
+os.system('python SEC-4-2-HELPER.py --dataset celeba --metric dp --seed 42')
+os.system('python SEC-4-2-HELPER.py --dataset nlp --metric dp --seed 42')
 
 
-os.system('python SEC-4-2.py --dataset adult --metric dp --seed 42 --model_type nn')
-os.system('python SEC-4-2.py --dataset bank --metric dp --seed 42 --model_type nn')
-os.system('python SEC-4-2.py --dataset celeba --metric dp --seed 42 --model_type nn')
-os.system('python SEC-4-2.py --dataset nlp --metric dp --seed 42 --model_type nn')
+os.system('python SEC-4-2-HELPER.py --dataset adult --metric dp --seed 42 --model_type nn')
+os.system('python SEC-4-2-HELPER.py --dataset bank --metric dp --seed 42 --model_type nn')
+os.system('python SEC-4-2-HELPER.py --dataset celeba --metric dp --seed 42 --model_type nn')
+os.system('python SEC-4-2-HELPER.py --dataset nlp --metric dp --seed 42 --model_type nn')
